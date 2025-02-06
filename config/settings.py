44b4a4cdcc1fc4986c0dccceeed92f2dc2e2b193
Alexey Dyakonov <dyakonov261@gmail.com>
2025-02-06 22:24:35 +0500

    docker1

--- config/settings.py
+++ config/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.getenv("POSTGRES_NAME"),
-        "USER": 'alexey', #os.getenv("POSTGRES_USER"),
+        "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": os.getenv("POSTGRES_PORT"),
